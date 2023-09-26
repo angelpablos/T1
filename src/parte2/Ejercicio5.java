233cfe3226eb2763aed3c855b5ec2a4284bdc0a7
apablos <apablos@106-21.info2>
2023-09-26 09:48:52 +0200

    ejercicio 5

--- src/parte2/Ejercicio5.java
+++ src/parte2/Ejercicio5.java
@@ -12,6 +12,13 @@ public class Ejercicio5 {
 		
 		System.out.println("Introduce la nota del segundo trimestre: ");
 		int segundoTrimestre = sc.nextInt();
+		
+		System.out.println("Introduce la nota del tercer trimestre: ");
+		int tercerTrimestre = sc.nextInt();
+		
+		int suma = primerTrimestre + segundoTrimestre + tercerTrimestre;
+		System.out.println("Nota en el boletín: " + (suma / 3));
+		System.out.println("Nota en el expediente: " + ((float) suma / 3));
 
 		sc.close();
 	}
